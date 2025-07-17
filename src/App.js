8ba451865a0caeaa78a70ee7d9e3c0135b4bca79
Peter Howland <peter@objective.work>
2025-07-17 01:13:37 -0400

    Fixed references page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import ContactPage from './components/view/pages/Contact'
 import ThankYouPage from './components/view/pages/ThankYou'
 import PortfolioPage from './components/view/pages/Portfolio'
 import ResumePage from './components/view/pages/Resume'
-import RecommendationsPage from './components/view/pages/References'
+import ReferencesPage from './components/view/pages/References'
 import styled from 'styled-components'
 
 const AppContainer = styled.div`
@@ -54,7 +54,7 @@ const App = () => (
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/thank-you" element={<ThankYouPage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
-          <Route path="/recommendations" element={<RecommendationsPage />} />
+          <Route path="/references" element={<ReferencesPage />} />
           <Route path="/resume" element={<ResumePage {...resumeContent} />} />
         </Routes>
       </Content>
